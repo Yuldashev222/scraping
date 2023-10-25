1ff9687330519eff9c503b1b407a7125e6ecac66
yuldashev222 <oybekyuldashov54@gmail.com>
2023-10-25 14:57:03 +0500

    add new-pdfs

--- src/main/tasks.py
+++ src/main/tasks.py
@@ -387,6 +387,8 @@ def extract_url_pdf(webpage_url, inform_id):
                 if os.path.isfile(save_path + '.first_page.pdf'):
                     os.remove(save_path + '.first_page.pdf')
                 print(e)
+                last_pdf = None
+                new_pdfs = False
 
         else:
             location = f'{settings.MEDIA_ROOT}/{inform.country}/{inform.region}/test/'
@@ -462,6 +464,8 @@ def extract_url_pdf(webpage_url, inform_id):
                 if os.path.isfile(save_path + '.first_page.pdf'):
                     os.remove(save_path + '.first_page.pdf')
                 print(e)
+                last_pdf = None
+                new_pdfs = False
 
     inform.is_completed = True
     inform.new_pdfs = new_pdfs
