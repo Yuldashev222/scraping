3885317afa84080f03ca5ded972e0248c7b2cc64
yuldashev222 <oybekyuldashov54@gmail.com>
2023-11-16 12:03:20 +0500

    asd

--- src/analytics/models.py
+++ src/analytics/models.py
@@ -7,9 +7,43 @@ class VisitorAnalytics(models.Model):
     def __str__(self):
         return 'changeme'
 
+    class Meta:
+        verbose_name_plural = ' VisitorAnalytics'
+
 
 class CitiesAnalytics(models.Model):
     country = models.CharField(max_length=300)
     city = models.CharField(max_length=300)
     visitors = models.IntegerField()
     range_date = models.DateField(null=True)
+
+    class Meta:
+        verbose_name_plural = ' CitiesAnalytics'
+
+
+class PagesAnalytics(models.Model):
+    page_path = models.CharField(max_length=500)
+    visitors = models.IntegerField()
+    range_date = models.DateField(null=True)
+
+    class Meta:
+        verbose_name_plural = ' PagesAnalytics'
+
+
+class DeviceAnalytics(models.Model):
+    category = models.CharField(max_length=500)
+    model = models.CharField(max_length=500)
+    visitors = models.IntegerField()
+    range_date = models.DateField(null=True)
+
+    class Meta:
+        verbose_name_plural = ' DeviceAnalytics'
+
+
+class ChannelAnalytics(models.Model):
+    channel = models.CharField(max_length=500)
+    visitors = models.IntegerField()
+    range_date = models.DateField(null=True)
+
+    class Meta:
+        verbose_name_plural = ' ChannelAnalytics'
